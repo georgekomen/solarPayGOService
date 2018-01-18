a0bed55614e7cb3d9dbd116c8f40a50d134b84fb
spotless\Hacker-pc <gkkomensi@gmail.com>
2018-01-18 06:24:37 +0300

    #stop pulling uninstalled customers

--- sunamiapi/Controllers/api/CustomersController.cs
+++ sunamiapi/Controllers/api/CustomersController.cs
@@ -87,7 +87,7 @@ namespace sunamiapi.Controllers.api
         [HttpPost]
         public List<paymentRatesClassPerClient> GetPaymentInactiveRates([FromBody] JArray value)
         {
-            DateTime end1;
+           /* DateTime end1;
             db_a0a592_sunamiEntities se = new db_a0a592_sunamiEntities();
 
             List<paymentRatesClassPerClient> list = new List<paymentRatesClassPerClient>();
@@ -113,7 +113,7 @@ namespace sunamiapi.Controllers.api
                 list = calcInvoiceBtwnDatesm(beginDate, end1, list2).OrderByDescending(g => g.Percent).ToList();
             }
             se.Dispose();
-            return list;
+            return list;*/
             return null;
         }
 
