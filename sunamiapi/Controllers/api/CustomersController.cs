00ae4e57adebe44677ee2b3759b01bc14ee3994a
George Komen <g.komen@carepay.co.ke>
2018-03-05 08:07:21 +0300

    reverse

--- sunamiapi/Controllers/api/CustomersController.cs
+++ sunamiapi/Controllers/api/CustomersController.cs
@@ -643,62 +643,7 @@ namespace sunamiapi.Controllers.api
                 ts.last_connected_to_db_date = DateTime.Now;
                 tc = se.tbl_customer.FirstOrDefault(oo => oo.customer_id == ts.customer_id);
             }
-            if (switchResponse.Status == "0")
-            {
-                try
-                {
-                    //switch off
-                    ts.active_status = true;
-                    tbl_switch_logs sl = new tbl_switch_logs();
-                    sl.customer_id = ts.customer_id;
-                    sl.switched_off_by = "";
-                    sl.switch_off_date = DateTime.Today;
-                    try
-                    {
-                        List<tbl_customer> lst = new List<tbl_customer>();
-                        lst.Add(tc);
-                        List<paymentRatesClassPerClient> res1 = calcInvoiceBtwnDatesm(beginDate, DateTime.Today, lst);
-                        sl.switch_off_payrate = res1[0].Percent.Value.ToString();
-                    }
-                    catch
-                    {
-                        sl.switch_off_payrate = "0";
-                    }
-                    se.tbl_switch_logs.Add(sl);
-                }
-                catch (Exception g)
-                {
-                    res = g.Message;
-                }
-            }
-            else if (switchResponse.Status == "1")
-            {
-                try
-                {
-                    //switch on
-                    ts.active_status = false;
-                    int sl1 = se.tbl_switch_logs.Where(h => h.customer_id == ts.customer_id).Max(j => j.Id);
-                    tbl_switch_logs sl = se.tbl_switch_logs.FirstOrDefault(h => h.customer_id == ts.customer_id && h.Id == sl1);
-                    sl.customer_id = ts.customer_id;
-                    sl.switched_on_by = " ";
-                    sl.switch_on_date = DateTime.Today;
-                    try
-                    {
-                        List<tbl_customer> lst = new List<tbl_customer>();
-                        lst.Add(tc);
-                        List<paymentRatesClassPerClient> res1 = calcInvoiceBtwnDatesm(beginDate, DateTime.Today, lst);
-                        sl.switch_on_payrate = res1[0].Percent.Value.ToString();
-                    }
-                    catch
-                    {
-                        sl.switch_on_payrate = "0";
-                    }
-                }
-                catch (Exception g)
-                {
-                    res = g.Message;
-                }
-            }
+            
             se.SaveChanges();
             se.Dispose();
         }
@@ -724,6 +669,9 @@ namespace sunamiapi.Controllers.api
                 string res;
                 try
                 {
+                    //switch off
+                    ts.active_status = true;
+
                     if (allowsendsms == true)
                     {
                         //notify the customer that he has been put on
@@ -733,6 +681,23 @@ namespace sunamiapi.Controllers.api
                         ss.sendSmsThroughGateway(sim_no, "smsc$1%$+254713014492%smsd$" + customernames[0] + "%s*solar yako imezimwa#e", customer_id);
                         //ss.sendSmsThroughGateway(tc.phone_numbers, customernames[0] + ", Sunami solar inakujulisha kuwa solar yako imewashwa", customer_id);
                     }
+
+                    tbl_switch_logs sl = new tbl_switch_logs();
+                    sl.customer_id = customer_id;
+                    sl.switched_off_by = loogeduser;
+                    sl.switch_off_date = DateTime.Today;
+                    try
+                    {
+                        List<tbl_customer> lst = new List<tbl_customer>();
+                        lst.Add(tc);
+                        List<paymentRatesClassPerClient> res1 = calcInvoiceBtwnDatesm(beginDate, DateTime.Today, lst);
+                        sl.switch_off_payrate = res1[0].Percent.Value.ToString();
+                    }
+                    catch
+                    {
+                        sl.switch_off_payrate = "0";
+                    }
+                    se.tbl_switch_logs.Add(sl);
                     res = "";
                 }
                 catch (Exception g)
@@ -745,6 +710,7 @@ namespace sunamiapi.Controllers.api
             {
                 return null;
             }
+
         }
 
         public string switchOn(tbl_system ts, string sim_no, db_a0a592_sunamiEntities se, string customer_id, string loogeduser)
@@ -755,6 +721,9 @@ namespace sunamiapi.Controllers.api
                 string res;
                 try
                 {
+                    //switch on
+                    ts.active_status = false;
+
                     if (allowsendsms == true)
                     {
                         //notify the customer that he has been switched off
@@ -764,6 +733,23 @@ namespace sunamiapi.Controllers.api
                         ss.sendSmsThroughGateway(sim_no, "smsc$0%$+254713014492%smsd$" + customernames[0] + "%s*solar yako imewashwa#e", customer_id);
                         //ss.sendSmsThroughGateway(tc.phone_numbers, customernames[0] + ", Sunami solar inakujulisha kuwa solar yako inazimwa leo kutokana na deni. Tafadhali lipa ili iwashwe tena", customer_id);
                     }
+                    int sl1 = se.tbl_switch_logs.Where(h => h.customer_id == customer_id).Max(j => j.Id);
+                    tbl_switch_logs sl = se.tbl_switch_logs.FirstOrDefault(h => h.customer_id == customer_id && h.Id == sl1);
+                    sl.customer_id = customer_id;
+                    sl.switched_on_by = loogeduser;
+                    sl.switch_on_date = DateTime.Today;
+                    try
+                    {
+                        List<tbl_customer> lst = new List<tbl_customer>();
+                        lst.Add(tc);
+                        List<paymentRatesClassPerClient> res1 = calcInvoiceBtwnDatesm(beginDate, DateTime.Today, lst);
+                        sl.switch_on_payrate = res1[0].Percent.Value.ToString();
+                    }
+                    catch
+                    {
+                        sl.switch_on_payrate = "0";
+                    }
+
                     res = "";
                 }
                 catch (Exception g)
@@ -778,6 +764,7 @@ namespace sunamiapi.Controllers.api
                 return null;
             }
         }
+
 
         /*public List<object> getmpesaPayments()
         {
